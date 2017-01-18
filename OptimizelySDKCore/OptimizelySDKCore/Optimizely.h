e3281dcd7cd84969d9c5935b652bcfee358a779c
Alda Luong <alda@optimizely.com>
2017-01-18 10:49:57 -0800

    Added test commetn.

--- OptimizelySDKCore/OptimizelySDKCore/Optimizely.h
+++ OptimizelySDKCore/OptimizelySDKCore/Optimizely.h
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and      *
  * limitations under the License.                                           *
  ***************************************************************************/
-
+//alda test
 #import <Foundation/Foundation.h>
 #import "OPTLYBuilder.h"
 
